ee685ca72fb5884331b46d2ba280e752d639cbca
LazyUnigineDude <lazy.unigine@gmail.com>
2023-05-12 21:07:59 -0400

    Add

---
+++ data/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "4b5426aa5db6a601faf2c76c2fc41bdb84e09f7e")]
+public class MainMenu : Component
+{
+	private void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	private void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
