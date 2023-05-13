25febfcff43a09bdef288fc8310b6a5822dcaeda
LazyUnigineDude <lazy.unigine@gmail.com>
2023-05-12 21:10:13 -0400

    Add

--- data/Scripts/Menu/Menu.cs
+++ data/Scripts/Menu/Menu.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unigine;
 
-[Component(PropertyGuid = "4b5426aa5db6a601faf2c76c2fc41bdb84e09f7e")]
-public class MainMenu : Component
+[Component(PropertyGuid = "364df5f913b6a70452a50693d3c9dad6c9c6543d")]
+public class Menu : Component
 {
 	private void Init()
 	{
